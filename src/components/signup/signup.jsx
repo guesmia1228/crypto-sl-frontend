cffb7e0dcddbd66c8c1d1dd70a388875d987309e
Guesmia Abdelmadjid <guesmiaabdelmadjid6@gmail.com>
2023-05-05 17:03:26 +0200

    add language

--- src/components/signup/signup.jsx
+++ src/components/signup/signup.jsx
@@ -6,7 +6,7 @@ import styles from "./signup.module.css";
 import { Link, useNavigate } from "react-router-dom";
 import { useEffect, useState } from "react";
 import { useTranslation } from "react-i18next";
-import backendAPI from "../../api/backendAPI"
+import backendAPI from "../../api/backendAPI";
 
 const Signup = () => {
   const { t } = useTranslation();
@@ -18,17 +18,17 @@ const Signup = () => {
   const [Email, setEmail] = useState("");
   const [Password, setPassword] = useState("");
   const [UseOption, setUseOption] = useState("Choose Options");
+  const [LangOption, setLangOption] = useState("Choose Language");
   const api = new backendAPI();
 
-    const resetForm = () => {
-      setFirstName("");
-      setLastName("");
-      setTelefon("");
-      setEmail("");
-      setPassword("");
-      setUseOption("Choose Options");
-    }
-    
+  const resetForm = () => {
+    setFirstName("");
+    setLastName("");
+    setTelefon("");
+    setEmail("");
+    setPassword("");
+    setUseOption("Choose Options");
+  };
 
   async function submitForm() {
     const requestData = {
@@ -45,91 +45,95 @@ const Signup = () => {
     const response = await api.register(requestData);
     if (response == null) {
       setErrorMessage("Error when registering");
-    }else{
-      setMessage("Please confirm your email address to proceed.");  
+    } else {
+      setMessage("Please confirm your email address to proceed.");
     }
-  
-}
+  }
 
-function handleClick() {
-  submitForm();
-}
+  function handleClick() {
+    submitForm();
+  }
 
-return (
-  <div className={styles.signup}>
-    <div className={styles.left}>
-      <img src={Logo} alt="" />
+  return (
+    <div className={styles.signup}>
+      <div className={styles.left}>
+        <img src={Logo} alt="" />
 
-      <div>
-        <h2>
-          {t("signUp.titleP1")}
-          <br />
-          {t("signUp.titleP2")}
-        </h2>
-        <p>{t("signUp.description")}</p>
+        <div>
+          <h2>
+            {t("signUp.titleP1")}
+            <br />
+            {t("signUp.titleP2")}
+          </h2>
+          <p>{t("signUp.description")}</p>
 
-        <p>
-          {t("signUp.info")}
-          <u>
-            <Link to="/login">{t("signUp.infoButton")}</Link>
-          </u>
-        </p>
+          <p>
+            {t("signUp.info")}
+            <u>
+              <Link to="/login">{t("signUp.infoButton")}</Link>
+            </u>
+          </p>
+        </div>
       </div>
-    </div>
 
-    <div className={styles.right}>
-      {errorMessage && (
-        <div className={styles.errormessagecontainer}>
-          <p>{errorMessage}</p>
+      <div className={styles.right}>
+        {errorMessage && (
+          <div className={styles.errormessagecontainer}>
+            <p>{errorMessage}</p>
+          </div>
+        )}
+        {message && (
+          <div className={styles.messagecontainer}>
+            <p>{message}</p>
+          </div>
+        )}
+        <div className={styles.row}>
+          <Input
+            label={t("signUp.firstNameLabel")}
+            placeholder={t("signUp.firstNamePlaceholder")}
+            value={FirstName}
+            setState={setFirstName}
+          />
+          <Input
+            label={t("signUp.lastNameLabel")}
+            placeholder={t("signUp.lastNamePlaceholder")}
+            value={LastName}
+            setState={setLastName}
+          />
         </div>
-      )}
-      {message && (
-        <div className={styles.messagecontainer}>
-          <p>{message}</p>
-        </div>
-      )}
-      <div className={styles.row}>
         <Input
-          label={t("signUp.firstNameLabel")}
-          placeholder={t("signUp.firstNamePlaceholder")}
-          value={FirstName}
-          setState={setFirstName}
+          label={t("signUp.telefonLabel")}
+          placeholder="(979) 268-4143"
+          value={Telefon}
+          setState={setTelefon}
         />
         <Input
-          label={t("signUp.lastNameLabel")}
-          placeholder={t("signUp.lastNamePlaceholder")}
-          value={LastName}
-          setState={setLastName}
+          label={t("signUp.emailLabel")}
+          placeholder={t("signUp.emailPlaceholder")}
+          value={Email}
+          setState={setEmail}
         />
-      </div>
-      <Input
-        label={t("signUp.telefonLabel")}
-        placeholder="(979) 268-4143"
-        value={Telefon}
-        setState={setTelefon}
-      />
-      <Input
-        label={t("signUp.emailLabel")}
-        placeholder={t("signUp.emailPlaceholder")}
-        value={Email}
-        setState={setEmail}
-      />
-      <Input
-        label={t("signUp.passwordLabel")}
-        placeholder={t("signUp.passwordPlaceholder")}
-        value={Password}
-        setState={setPassword}
-        secure
-      />
-      <Options value={UseOption} setValue={setUseOption} />
-      <Button className={styles.button} onClick={handleClick}>
-        {t("signUp.formButton")}
-      </Button>
+        <Input
+          label={t("signUp.passwordLabel")}
+          placeholder={t("signUp.passwordPlaceholder")}
+          value={Password}
+          setState={setPassword}
+          secure
+        />
+        <Options
+          value={LangOption}
+          setValue={setLangOption}
+          options={["English", "German", "French"]}
+        />
+        <Options value={UseOption} setValue={setUseOption} />
+        <Button className={styles.button} onClick={handleClick}>
+          {t("signUp.formButton")}
+        </Button>
 
-      <p>{t("signUp.formInfo")}</p>
+        <p>{t("signUp.formInfo")}</p>
+      </div>
     </div>
-  </div>
-);
+  );
 };
 
 export default Signup;
