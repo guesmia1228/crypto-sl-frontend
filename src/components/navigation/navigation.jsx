90beb32099aee6fdb8837088fccfb3d9859bc657
Guesmia Abdelmadjid <guesmiaabdelmadjid6@gmail.com>
2023-07-16 12:59:15 +0200

    Remove submenu "solutions" in mobile view

--- src/components/navigation/navigation.jsx
+++ src/components/navigation/navigation.jsx
@@ -3,30 +3,20 @@ import styles from "./navigation.module.css";
 import Logo from "../../assets/logo/logo.svg";
 
 import Hamburger from "../../assets/icon/hamburger.svg";
-import DropDown from "../../assets/icon/dropdown.svg";
 import Button from "../button/button";
 import Languages from "./languages.jsx/languages";
 import { useState } from "react";
 
 import QR from "../../assets/icon/qrcode.svg";
 
-import Payment from "../../assets/icon/money.svg";
-import Cash from "../../assets/icon/cash.svg";
-
-import PaymentW from "../../assets/icon/moneyW.svg";
-import CashW from "../../assets/icon/cashW.svg";
 import { Link } from "react-router-dom";
-
-import Arrow from "../../assets/icon/blueArrow.svg";
 
 import { useTranslation } from "react-i18next";
 
 const Navigation = () => {
-  const { t, i18n } = useTranslation();
+  const { t } = useTranslation();
 
   const [openMenu, setOpenMenu] = useState(false);
-
-  const [openDrop, setOpenDrop] = useState(false);
 
   return (
     <nav className={`${styles.navigation} load`}>
@@ -94,57 +84,10 @@ const Navigation = () => {
                 {t("navigation.home")}
               </Link>
             </li>
-            <li
-              className={`standard ${styles.hover} ${styles.mobItem}`}
-              style={{ height: openDrop ? 220 : 30 }}
-            >
-              <div
-                className={styles.menu}
-                onClick={() => setOpenDrop((prev) => !prev)}
-              >
-                {t("navigation.solutions")}{" "}
-                <img
-                  style={{
-                    transform: openDrop ? "rotate(180deg)" : "rotate(0deg)",
-                  }}
-                  src={DropDown}
-                  alt=""
-                />
-              </div>
-              <div className={`${styles.mobDown}`}>
-                <div className={`${styles.mobListContent}`}>
-                  <Link
-                    to="/payment"
-                    className={styles.item}
-                    onClick={() => setOpenMenu(false)}
-                  >
-                    <img src={Payment} alt="" />
-                    <div>
-                      <p className={styles.headline}>
-                        {t("navigation.payment")} <img src={Arrow} alt="" />
-                      </p>
-                      <p className={styles.subheadline}>
-                        {t("navigation.paymentDescription")}
-                      </p>
-                    </div>
-                  </Link>
-                  <Link
-                    to="/payroll"
-                    className={styles.item}
-                    onClick={() => setOpenMenu(false)}
-                  >
-                    <img src={Cash} alt="" />
-                    <div>
-                      <p className={styles.headline}>
-                        {t("navigation.payroll")} <img src={Arrow} alt="" />
-                      </p>
-                      <p className={styles.subheadline}>
-                        {t("navigation.payrollDescription")}
-                      </p>
-                    </div>
-                  </Link>
-                </div>
-              </div>
+            <li className="standard">
+              <Link to="/payment" onClick={() => setOpenMenu(false)}>
+                {t("navigation.solutions")}
+              </Link>
             </li>
             <li className="standard">
               <Link to="/support" onClick={() => setOpenMenu(false)}>
