bfb2148b4732493ec25836f474ea0239b0b258c8
Guesmia Abdelmadjid <guesmiaabdelmadjid6@gmail.com>
2023-08-09 15:47:17 +0200

    Buttons with color "black", "white" and regular color

--- src/components/button/button.jsx
+++ src/components/button/button.jsx
@@ -2,25 +2,42 @@ import { Link } from "react-router-dom";
 import styles from "./button.module.css";
 
 const Button = ({ children, className, color, link, onClick }) => {
+	let backgroundColor = "linear-gradient(94.15deg, #0784B5 -27.47%, #66BFDE 118.26%)";
+	if (color === "white") {
+		backgroundColor = "#fff";
+	} else if (color === "black") {
+		backgroundColor = "#101010";
+	}
+
+	let fontColor = "#fff";
+	if (color === "white") {
+		fontColor = "#000";
+	} else if (color === "black") {
+		fontColor = "#fff";
+	}
+
+	let border = "0px solid transparent";
+	if (color === "black") {
+		border = "1px solid var(--white-color)";
+	}
+
   return (
     <div
       className={`${styles.button} ${className}`}
       onClick={onClick}
       style={{
-        background:
-          color === "white"
-            ? "#fff"
-            : "linear-gradient(94.15deg, #0784B5 -27.47%, #66BFDE 118.26%)",
+        background: backgroundColor,
+		border: border,
       }}
     >
       {link ? (
         <Link to={link}>
-          <div style={{ color: color === "white" ? "#000" : "#fff" }}>
+          <div style={{ color: fontColor}}>
             {children}
           </div>
         </Link>
       ) : (
-        <div style={{ color: color === "white" ? "#000" : "#fff" }}>
+        <div style={{ color: fontColor }}>
           {children}
         </div>
       )}
